f79dd04a54bf8ff533ca997e0f40bf9d24d76ad2
lannguyen <nguyentruongngoclan@gmail.com>
2023-01-29 00:14:14 -0800

    Add transfer api

--- util/random.go
+++ util/random.go
@@ -42,7 +42,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{EUR, CAD, USD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
